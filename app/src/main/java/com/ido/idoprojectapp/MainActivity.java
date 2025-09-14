8df79a9dfb27635f72202563415997e0d5a9558c
Ttgame007 <ido999k@gmail.com>
2025-09-14 17:32:31 +0300

    added UI Sign in screen welcome screen and intents netween them

---
+++ app/src/main/java/com/ido/idoprojectapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ido.idoprojectapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signIn;
+    Button signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        signIn = findViewById(R.id.signIn);
+        signUp = findViewById(R.id.signUpText);
+        signUp.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SignUp.class);
+            startActivity(intent);
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
